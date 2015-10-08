669a6465c1943d90da26abda98b23aea0defcb42
Frank Borkin <francis.borkin@globallogic.com>
2015-10-08 08:05:10 +0300

    Move inline JS to external file.

--- www/js/index.js
+++ www/js/index.js
@@ -1,78 +1,87 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+            var me;
+            var map;
+            var marker;
+            var markers = [];
+            var lat;
+            var lon;
 
-// onSuccess Callback
-// This method accepts a Position object, which contains the
-// current GPS coordinates
-//
-var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-};
+            function getData()
+            {
+                var o = {};
+                var positions = [];
+                o.lat = lat; 
+                o.lon = lon;
+                o.causes = $( "#causes" ).val() || [];
+                o.items = $( "#items" ).val() || [];
 
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
+                //Quick AJAX call
+                $.getJSON("https://www.give-anywhere.xyz/service.php",o).done(function( data ) {
+                    //Remove markers from the page
+                    for (var m = 0; m < markers.length; m++) {
+                        markers[m].setMap(null);
+                    }
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+                    //Then clean the references to them
+                    while(markers.length > 0)
+                    {
+                        markers.pop();
+                    }
 
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-        
-        
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+                    //Now create a new position and marker for each entry in the result
+                    $.each( data, function( i, point )
+                           {
+                        var pos = new google.maps.LatLng(point.lat,point.lon);
+                        positions.push(pos);
+                        marker = new google.maps.Marker({ position: pos, map: map, title: point.name, icon: 'img/'+point.type+'.png' });
+                        markers.push(marker);
+                        data[i].dis = (data[i].distance/1000).toFixed(1);
+                    });
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+                    //  Create a new viewpoint bound
+                    var bounds = new google.maps.LatLngBounds ();
+                    //Show myself
+                    bounds.extend(me);
+                    //  Go through .the other markers...
+                    for (var i = 0; i < markers.length; i++) {
+                        //  And increase the bounds to take this point
+                        bounds.extend (positions[i]);
+                    }
+                    //  Fit these bounds to the map
+                    map.fitBounds (bounds);
 
-        console.log('Received Event: ' + id);
-        var div = document.getElementById("map_canvas");
-        map = plugin.google.maps.Map.getMap(div);
-    }
-};
+                    //Update the accordion
+                    $("#accordion").empty();
+                    $.each( data, function( i, point )
+                           {
+                        $("#accordion").append('<div class="panel panel-default"><div class="panel-heading" style="height: 50px; line-height: 50px; position: relative; vertical-align: middle; padding: 0 0 0 15px;" role="tab" id="heading'+i+'"><a data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'"><span class="panel-title" id="panel'+i+'">'+point.name+' ('+point.dis+' km)</span></a></div><div id="collapse'+i+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading'+i+'"><div class="panel-body">'+point.desc+'<hr><strong>Looking for:</strong><br>'+point.want+'<br><strong>Current Status:</strong><br>'+point.havetxt+'<br><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="'+point.wantpc+'" aria-valuemin="0" aria-valuemax="100" style="width: '+point.wantpc+'%;">'+point.wantpc+'%</div></div></div></div></div>');
+                        $("#heading"+i).append('<ul id="list'+i+'" style="padding: 0; margin: 0; list-style-type: none; float: right; line-height: 50px; overflow: hidden;"></ul>');
+                        $("#list"+i).append('<li style="float: right"><a data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'" style="display: block; width: 50px; background-color: #dddddd; text-align: center"><i class="fa fa-chevron-down"></i></a></li>');
+                        $("#list"+i).append('<li style="float: right"><a data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'" style="display: block; width: 50px;"><img src="img/'+point.type+'_big.png" height="40"></a></li>');
+
+                    });
+                });
+            }
+
+            function onErrorGeo(error) {  
+                alert("Could not fix position.");
+            }
+
+            function onSucGeo(position) {
+                me = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                map.setCenter(me);
+                ///FIXME: i18n me.
+                marker = new google.maps.Marker({ position: me, map: map, title: 'Me' });
+                lat = position.coords.latitude;
+                lon = position.coords.longitude;
+            }
+
+            function main() {
+                var mapOptions = { disableDefaultUI: true, zoom: 6, styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}] };
+                map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+                navigator.geolocation.getCurrentPosition(onSucGeo, onErrorGeo, {timeout: 10000, enableHighAccuracy: true });
+                $( "#causes" ).change(getData);
+                $( "#items" ).change(getData);
+                getData();
+            }
+
+            document.addEventListener("deviceready", main, false);
